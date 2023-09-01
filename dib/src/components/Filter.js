7faf1d7f780ee6a888ca3faa44849442ea1239ab
Olimpiya Obreshkova <ollydimitrova94@gmail.com>
2023-09-01 21:54:44 +0200

    components modified

--- dib/src/components/Filter.js
+++ dib/src/components/Filter.js
@@ -1,57 +1,42 @@
-import React, { useState } from 'react';
-import { IoStar } from 'react-icons/io5';
+import { useState } from 'react';
 import './Filter.css';
 
-function Filter() {
-  const [priceFilter, setPriceFilter] = useState('Price');
-  const [ratingFilter, setRatingFilter] = useState(null);
-  const [showPriceDropdown, setShowPriceDropdown] = useState(false);
-  const [showRatingDropdown, setShowRatingDropdown] = useState(false);
+function Filter({ 
+  initialLabel = 'Select', 
+  initialOption = null, 
+  options = [], 
+  renderOption = (option) => option, 
+  onSelect 
+}) {
+  const [selectedOption, setSelectedOption] = useState(initialOption);
+  const [showDropdown, setShowDropdown] = useState(false);
 
-const handlePriceSelection = (value) => {
-    setPriceFilter(value);
-    setShowPriceDropdown(false);
-};
+  const handleSelection = (value) => {
+    setSelectedOption(value);
+    setShowDropdown(false);
+    if (onSelect) onSelect(value);
+  };
 
-const handleRatingSelection = (value) => {
-    setRatingFilter(value);
-    setShowRatingDropdown(false);
-};
-
-const renderStars = (num) => {
-    let stars = [];
-    for (let i = 0; i < num; i++) {
-      stars.push(<IoStar key={i} />);
-    }
-    return <div className="star-wrapper">{stars}</div>;
-};
-
-return (
+  return (
     <div className="filter-container">
       <div className="dropdown">
-        <button onClick={() => setShowPriceDropdown(!showPriceDropdown)} className="dropdown-button">
-          {priceFilter} <span className="arrow">▼</span>
+        <button 
+          onClick={() => setShowDropdown(!showDropdown)} 
+          className="dropdown-button"
+        >
+          {selectedOption ? renderOption(selectedOption) : initialLabel}
+          {selectedOption ? null : <span className="arrow">▼</span>}
         </button>
-        <div className="dropdown-content" style={{ display: showPriceDropdown ? 'block' : 'none' }}>
-          <button onClick={() => handlePriceSelection('Affordable')}>Affordable</button>
-          <button onClick={() => handlePriceSelection('Medium')}>Medium</button>
-          <button onClick={() => handlePriceSelection('High')}>High</button>
+        <div className="dropdown-content" style={{ display: showDropdown ? 'block' : 'none' }}>
+          {options.map((option, index) => (
+            <button key={index} onClick={() => handleSelection(option)}>
+              {renderOption(option)}
+            </button>
+          ))}
         </div>
       </div>
-      <div className="dropdown">
-      <button onClick={() => setShowRatingDropdown(!showRatingDropdown)} className="dropdown-button">
-        {ratingFilter ? renderStars(ratingFilter) : <>Rating <span className="iconSpacing"><IoStar /></span></>}
-      </button>
-      <div className="dropdown-content" style={{ display: showRatingDropdown ? 'block' : 'none' }}>
-        <div className="star-row"><button onClick={() => handleRatingSelection(1)}>{renderStars(1)}</button></div>
-        <div className="star-row"><button onClick={() => handleRatingSelection(2)}>{renderStars(2)}</button></div>
-        <div className="star-row"><button onClick={() => handleRatingSelection(3)}>{renderStars(3)}</button></div>
-        <div className="star-row"><button onClick={() => handleRatingSelection(4)}>{renderStars(4)}</button></div>
-        <div className="star-row"><button onClick={() => handleRatingSelection(5)}>{renderStars(5)}</button></div>
-      </div>
     </div>
-  </div>
-);
+  );
 }
 
 export default Filter;
--- dib/src/pages/MainPage.js
+++ dib/src/pages/MainPage.js
@@ -1,28 +1,60 @@
-import NavBar from '../components/NavBar'
-import './MainPage.css'
-import SearchBar from '../components/SearchBar'
-import RestList from "../components/RestList"
-import Filter from "../components/Filter";
-import { FindButton } from '../components/Button';
+import NavBar from '../components/NavBar';
+import SearchBar from '../components/SearchBar';
+import RestList from '../components/RestList';
+import Filter from '../components/Filter';
+import Button from '../components/Button';
+import { IoStar } from 'react-icons/io5';
+import { GoSearch } from 'react-icons/go';
+import './MainPage.css';
 
-function MainPage () {
-  const handleClick = () => {
-    console.log("Button clicked");
+function MainPage() {
+
+  const handleClick = () => {};
+
+  const handlePriceSelect = (selectedPrice) => {
+    console.log("Selected price:", selectedPrice);
+  };
+
+  const handleRatingSelect = (selectedRating) => {
+    console.log("Selected rating:", selectedRating);
+  };
+
+  const renderStars = (num) => {
+    let stars = [];
+    for (let i = 0; i < num; i++) {
+      stars.push(<IoStar key={i} />);
+    }
+    return <div className="star-wrapper">{stars}</div>;
   };
 
   return (
     <div>
-      <NavBar/>
+      <NavBar />
       <h1 className="project-name">Drop into Berlin</h1>
       <div className="search-container">
         <SearchBar />
-        <FindButton onClick={handleClick} /> 
+        <Button secondary outline rounded onClick={handleClick}>
+          <GoSearch />
+          Find
+        </Button>
       </div>
-      <Filter/>
-      <h2 className='rest-list-header'>Some great Restaurants from around the city</h2>
-      <RestList/>
+      <div className="filter-wrapper">
+        <Filter
+          initialLabel="Price"
+          options={['Affordable', 'Medium', 'High']}
+          onSelect={handlePriceSelect}
+        />
+        <Filter
+          initialLabel={<><span>Rating</span><span className="iconSpacing"></span></>}
+          options={[1, 2, 3, 4, 5]}
+          renderOption={renderStars}
+          onSelect={handleRatingSelect}
+        />
+      </div>
+      <h2 className="rest-list-header">Some great Restaurants from around the city</h2>
+      <RestList />
     </div>
-  )
+  );
 }
 
 export default MainPage;
