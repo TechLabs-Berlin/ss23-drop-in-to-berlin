d1a64426196e17e257373ff7e54977f8db21a526
Phylanxy <98328473+Phylanxy@users.noreply.github.com>
2023-09-20 12:33:00 +0200

    first draft of flask API, need to confirm functionality & need to add html boilerplate

---
+++ API/flask_app.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, request, jsonify
+import pickle
+import joblib
+import sentence_transformers
+import pandas as pd
+
+app = Flask(__name__)
+
+# Load the model from .joblib file
+with open("NearestNeighbors_clf.joblib", "rb") as file:
+    loaded_model = joblib.load(file)
+
+# load the database from .csv file
+df = pd.read_csv("database_Leo.csv")
+
+# define embedding model based on all-MiniLM-L6-v2
+model = SentenceTransformer(
+    'sentence-transformers/all-MiniLM-L6-v2'
+    )
+model.max_seq_length = 512 # Change the length to 512
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    # extract input data from the request
+    star_rating = float(request.form["star_rating"])
+    price = float(request.form["price"])
+    user_input_str = str(request.form["user_input_str"])
+
+    # create embeddings for user input
+    user_input_emb = model.encode(user_input_str).reshape(1,-1)
+
+    # make prediction based on user input
+    distances, indices = loaded_model.kneighbors([user_input_emb])
+    # flatten indices array to use as index in dataframe
+    indices = indices.flatten()
+
+    # get ID's to return
+    recs = df.iloc[indices]["ID"]
+
+    return jsonify({"recommendations" : recs})
+
+if __name__ == "__main__":
+    app.run(debug=True)
