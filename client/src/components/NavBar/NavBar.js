132260a6b81ce1b2eeac91a78c693cac321dbde5
r1fel <felix.reiter@gmx.de>
2023-10-05 22:23:32 +0200

    full page refresh if logo clicked on home page

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -5,12 +5,16 @@ import Logo from '../../logo/logo-simple-text-small.svg'
 function NavBar () {
   const location = useLocation();
 
+  const refreshPage = () => {
+    window.location.reload();
+  };
 
+  const isHomePage = location.pathname === '/';
 
   return (
     <div className="nav-bar">
       <Link to={'/'}>
-        <img src={Logo} className='logo' />
+        <img src={Logo} className='logo' onClick={isHomePage ? refreshPage : null}/>
       </Link>
       <div>
         <Link to={'/about'} className="nav-item">About</Link>
