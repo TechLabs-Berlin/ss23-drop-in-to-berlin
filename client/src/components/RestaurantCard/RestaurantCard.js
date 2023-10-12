355dd56ffa531a23e082d89d95727c18109047c2
r1fel <felix.reiter@gmx.de>
2023-10-12 21:18:10 +0200

    fixed address on rest-card

--- client/src/components/RestaurantCard/RestaurantCard.js
+++ client/src/components/RestaurantCard/RestaurantCard.js
@@ -17,11 +17,11 @@ function RestCard({ rest }) {
     rest.photos && rest.photos.length > 0 && rest.photos[0].imageURL;
 
   // helper function to shorten the restaurant name
-  const name = shortenText(rest.name, 13);
+  const name = shortenText(rest.name, 17);
 
   let reviewText =
     rest.reviews && rest.reviews.length > 0 ? rest.reviews[0].text : '';
- 
+
   const [showFullReview, setShowFullReview] = useState(false);
 
   const isReviewTooLong = reviewText.length > 100;
@@ -33,7 +33,10 @@ function RestCard({ rest }) {
   const priceRange = rest.price_level ? '$'.repeat(rest.price_level) : null;
 
   const addressParts = rest.formatted_address?.split(', ');
-  const postalCode = addressParts.length >= 3 ? addressParts[addressParts.length - 2].replace('Berlin', '') : '';
+  const postalCode =
+    addressParts.length >= 3
+      ? addressParts[addressParts.length - 2].replace('*', '')
+      : '';
 
   return (
     <Link to={`/rest/${rest._id}`}>
@@ -44,19 +47,25 @@ function RestCard({ rest }) {
         )}
         <div className='card-content'>
           <div className='card-header'>
-            <h2 className='card-name'>{name}</h2>
-            <div className='card-rating'>
+            <div className='card-name-and-address-block'>
+              <h2 className='card-name'>{name}</h2>
+              {postalCode && (
+                <div className='card-postal-code'>
+                  <IoLocationOutline className='pin-icon' />
+                  {postalCode}
+                </div>
+              )}
+            </div>
+
+            <div className='card-price-and-rating-block'>
+              <div className='card-rating'>
+                <IoStar className='iostar' />
+                {rest.rating}
+              </div>
               {priceRange && <span className='card-price'>{priceRange}</span>}
-              <IoStar className='iostar' />
-              {rest.rating}
             </div>
           </div>
-          {postalCode && 
-            <div className='card-postal-code'>
-              <IoLocationOutline className='pin-icon'/> 
-              {postalCode}
-            </div>
-          }
+
           <p className='card-descr'>{reviewText}</p>
         </div>
       </div>
